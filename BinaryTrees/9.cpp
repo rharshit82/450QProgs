7d5a85fc52c05ad2b6cda8426694406d36548774
rharshit82 <35199148+rharshit82@users.noreply.github.com>
2021-07-02 21:39:09 +0530

    Update 9.cpp

--- BinaryTrees/9.cpp
+++ BinaryTrees/9.cpp
@@ -1,3 +1,4 @@
+#Recursive
 vector<int> output;
 void printLeftView(Node* root, int level, int* maxLevel){
     if(!root) return;
@@ -14,4 +15,25 @@ vector<int> leftView(Node *root)
     int maxLevel=0;
     printLeftView(root,1,&maxLevel);
     return output;
+}
+
+
+#Iterative
+vector<int> leftView(Node *root)
+{
+    vector<int> result;
+    if(!root) return result;
+    queue<Node*>q;
+    q.push(root);
+    while(!q.empty()){
+        int count=q.size();
+        for(int i=0;i<count;i++){
+            Node* temp=q.front();
+            q.pop();
+            if(i==0) result.push_back(temp->data);
+            if(temp->left) q.push(temp->left);
+            if(temp->right) q.push(temp->right);
+        }
+    }
+    return result;
 }
